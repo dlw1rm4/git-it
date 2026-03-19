ace40b50e5dcf75073f0d7b672addca9c1df18d8
Kaitlyn Nguy <kaitlyn.loc101@gmail.com>
2026-03-19 02:23:25 -0700

    title screen w/ animation and not working start button

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>welcome to</h1>
+      <h2>git-it!</h2>
+      <br></br>
+      <button>start</button>
+    </div>
+  )
+}
