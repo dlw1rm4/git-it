1557b0b76b2bd18d8a57058f6fb88aebc2813d42
Kaitlyn Nguy <kaitlyn.loc101@gmail.com>
2026-03-20 18:16:42 -0700

    added next page and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Lessons from './pages/lessons'
 import './App.css'
 
 export default function App() {
+  const navigate = useNavigate()
+
   return (
-    <div className="container">
-      <h1>welcome to</h1>
-      <h2>git-it!</h2>
-      <br></br>
-      <button>start</button>
-    </div>
+    <Routes>
+      <Route path="/" element={
+        <div className="container">
+        <h1>welcome to</h1>
+        <h2>git-it!</h2>
+        <br></br>
+        <button onClick={() => navigate ('/lessons')}>start</button>
+      </div>
+      } />
+      <Route path = "/lessons" element={<Lessons />} />
+    </Routes>
   )
 }
