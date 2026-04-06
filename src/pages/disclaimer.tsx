a8ff917b2d82df8b7687a278ff4e25a604d349e3
Kaitlyn Nguy <kaitlyn.loc101@gmail.com>
2026-04-06 01:38:01 -0700

    frontend for login/register/skip page

--- src/pages/disclaimer.tsx
+++ src/pages/disclaimer.tsx
@@ -8,7 +8,20 @@ export default function Disclaimer() {
     <div className="disclaimer-page">
         <p className="logo">git-it!</p>
         <div className="container">
-            <button onClick={() => navigate ('/lessons')}>start</button>
+          <div className="pop-up-box-container">
+            <div className="pop-up-box-header">
+              <div className="red-dot"></div>
+              <div className="header-text">Wait!</div>
+            </div>
+            <div className="pop-up-box">
+              <div className="box-text">Would you like to create an account to save your progress? (If you don't, you will lose your progress!)</div>
+              <div className="button-group">
+                <button className="btn-sure" onClick={() => navigate ('/lessons')}>Sure!</button>
+                <button className="btn-nah" onClick={() => navigate ('/lessons')}>Nah, we ball.</button>
+              </div>
+              <div className="login-link" onClick={() => navigate('/login')}>I already have an account.</div>
+            </div>
+          </div>
         </div>
     </div>
   )
