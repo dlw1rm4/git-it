214ff2c4b132c6feeb19bf8c9717b2012a1e3ae3
Kaitlyn Nguy <kaitlyn.loc101@gmail.com>
2026-04-07 01:07:59 -0700

    created frontend for signup and login

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,10 +1,48 @@
 import { useNavigate } from 'react-router-dom' 
+import { useState } from 'react';
 import './register.css'
+import logoIcon from '../images/logo.png'
 
 export default function Register() {
     const navigate = useNavigate()
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
     return(
-        <p className="logo">git-it!</p>
+        <div className="register-page">
+            <div className="logo-wrapper">
+                <img src={logoIcon} alt="logo" className="logo-icon"></img>
+                <span className="logo">git-it!</span>
+            </div>
+            <div className="container">
+            <div className="register-container">
+                <div className="register-header">
+                <div className="red-dot"></div>
+                <div className="header-text">Sign Up!</div>
+                </div>
+                <div className="register-box">
+                    <div className="box-text">Username</div>
+                    <input
+                        type="text"
+                        className="username-input"
+                        placeholder="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <div className="box-text">Password</div>
+                    <input
+                        type="text"
+                        className="password-input"
+                        placeholder="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <div className="button-group">
+                        <button className="signup-btn" onClick={() => navigate ('/register')}>Create</button>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
     )
 }
