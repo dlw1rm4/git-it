434905acb235a69d658ad0e947482f8c61f77f4e
Kaitlyn <kaitlyn.loc101@gmail.com>
2026-04-22 01:54:03 -0700

    terminal works

--- src/pages/l1terminal.tsx
+++ src/pages/l1terminal.tsx
@@ -14,7 +14,7 @@ export default function Lesson1() {
     const pages = [
         {
             title: "Commit 1: What is Git?",
-            text: "According to the Wikipedia, Git is a distributed version control software system that is capable of managing versions of source code or data. It is often used to control source code by programmers who are developing software collaboratively.\n\nIf you understood that, congrats! You're smarter than me :D !\n\nIf not, don't worry! Say you're playing a video game and reach a difficult dungeon. There's a good chance you might die, so before you enter the dungeon, you save your game. Git is basically a saving point for your project."
+            text: "According to the Wikipedia, Git is a distributed version control software system that is capable of managing versions of source code or data. It is often used to control source code by programmers who are developing software collaboratively.\n\nIf you understood that, congrats! You're smarter than me :D !\n\nIf not, don't worry! Say you're playing a video game and reach a difficult dungeon. There's a good chance you might die, so before you enter the dungeon, you save your game. Git is basically a saving point for your project, and GitHub stores everything we do in our Git project online."
         },
         {
             title: "Commit 1A: Create a repository",
@@ -25,6 +25,73 @@ export default function Lesson1() {
             text: "test2"
         }
     ]
+
+    const MOCK_FILES = {
+        'README.md': 'Welcome to git-it! This is a tutorial for Git.',
+        'hello.txt': 'Hello, world! You found a secret file.',
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            const input = terminalInput.trim();
+            const [command, ...args] = input.split(' '); 
+            const argument = args.join(' '); 
+            
+            let response = '';
+            const newLines = [...terminalLines, `> ${input}`];
+
+            switch (command) {
+                case 'echo':
+                    response = argument;
+                    break;
+
+                case 'cat':
+                    if (!argument) {
+                        response = 'cat: usage: cat <filename>';
+                    } else if (MOCK_FILES[argument as keyof typeof MOCK_FILES]) {
+                        response = MOCK_FILES[argument as keyof typeof MOCK_FILES];
+                    } else {
+                        response = `cat: ${argument}: No such file or directory`;
+                    }
+                    break;
+
+                case 'cd':
+                    if (!argument || argument === '.') {
+                        response = '';
+                    } else if (argument === '..') {
+                        response = 'Cannot go back; you are in the root directory.';
+                    } else {
+                        response = `cd: no such directory: ${argument}`;
+                    }
+                    break;
+
+                case 'ls':
+                    response = Object.keys(MOCK_FILES).join('    ');
+                    break;
+
+                case 'git':
+                    if (args[0] === 'init') {
+                        response = 'Initialized empty Git repository in /project/.git/';
+                    } else {
+                        response = `git: '${args[0]}' is not a git command. See 'git --help'.`;
+                    }
+                    break;
+
+                case '':
+                    break;
+
+                default:
+                    response = `command not found: ${command}`;
+            }
+
+            if (response) {
+                newLines.push(response);
+            }
+
+            setTerminalLines(newLines);
+            setTerminalInput('');
+        }
+    };
 
     const handleNext = () => {
         if (pageIndex < pages.length - 1)
@@ -90,8 +157,23 @@ export default function Lesson1() {
                         </div>
                     </div>
                     <div className="terminal">
-                        <p>random</p>
-                    </div>
+                        <div className="terminal-output">
+                            {terminalLines.map((line, index) => (
+                                <div key={index} className="terminal-line">{line}</div>
+                            ))}
+                        </div>
+                        <div className="terminal-input-line">
+                            <span className="prompt">{'>'}</span>
+                            <input
+                                type="text"
+                                className="terminal-input"
+                                value={terminalInput}
+                                onChange={(e) => setTerminalInput(e.target.value)}
+                                onKeyDown={handleKeyDown}
+                                autoFocus
+                            />
+                        </div>
+                </div>
                 </div>
             </div>
         </div>
