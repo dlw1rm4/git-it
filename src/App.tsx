bf02c42d14ff487133dc4d8728ee3c9c97b28193
dlw1rm4 <kaitlyn.loc101@gmail.com>
2026-04-30 11:01:22 -0700

    added wic logo and fixed overscroll

--- src/App.tsx
+++ src/App.tsx
@@ -5,31 +5,45 @@ import Register from './pages/register'
 import Login from './pages/login'
 import Lesson1 from './pages/l1terminal'
 import Lesson2 from './pages/l2terminal'
+import logo from './images/wic-logo.png'
 import './App.css'
 
 export default function App() {
   const navigate = useNavigate()
+  const logoStyle = {
+    position: 'fixed',
+    bottom: '20px',
+    right: '20px',
+    zIndex: 1000,
+  } as const;
 
   return (
-    <Routes>
-      <Route path="/" element={
-        <div className="container">
-        <h1>welcome to</h1>
-        <h2>git-it!</h2>
-        <br></br>
-        <button onClick={() => navigate ('/disclaimer')}>start</button>
+      <>
+      <div style={logoStyle}>
+        <a href="https://ucsdwic.github.io/" target="_blank" rel="noreferrer">
+          <img src={logo} alt="logo" style={{ width: '60px', height: '60px' }} />
+        </a>
       </div>
-      } />
-      <Route path = "/disclaimer" element={<Disclaimer />} />
-      <Route path = "/lessons" element={<Lessons />} />
-      <Route path = "/register" element={<Register />} />
-      <Route path = "/login" element={<Login />} />
-      <Route path = "/lesson1" element={<Lesson1 />} />
-      <Route path = "/lesson2" element={<Lesson2 />} />
-    </Routes>
-  )
-}
+      <Routes>
+        <Route path="/" element={
+          <div className="container">
+          <h1>welcome to</h1>
+          <h2>git-it!</h2>
+          <br></br>
+          <button onClick={() => navigate ('/disclaimer')}>start</button>
+        </div>
+        } />
+        <Route path = "/disclaimer" element={<Disclaimer />} />
+        <Route path = "/lessons" element={<Lessons />} />
+        <Route path = "/register" element={<Register />} />
+        <Route path = "/login" element={<Login />} />
+        <Route path = "/lesson1" element={<Lesson1 />} />
+        <Route path = "/lesson2" element={<Lesson2 />} />
+      </Routes>
+      </>
+    )
+  }
 
-// npm run dev
-// npm run build
-// npm run deploy
+  // npm run dev
+  // npm run build
+  // npm run deploy
